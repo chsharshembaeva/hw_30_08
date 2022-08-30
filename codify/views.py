9644f45d1dc7c359a9cd90f68e1df24f5f6c86d4
chsharshembaeva <chopona09@gmail.com>
2022-08-30 18:45:31 +0600

    First Commit

---
+++ codify/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render,  redirect
+from .models import Course, Student
+from django.http import HttpResponse
+from .forms import CourseForm, StudentForm
+
+
+def course_list(request):
+    courses = Course.objects.all()
+    return render(request, 'course_list.html', {'courses': courses})
+
+
+def course_add(request):
+    if request.method == "POST":
+        form = CourseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('course_list')
+        else:
+            return HttpResponse("Error")
+        # name = request.POST.get('name')
+        # mentor_name = request.POST.get('mentor_name')
+        # language = request.POST.get('language')
+        # if name and mentor_name and language:
+        #     course = Course.objects.create(name=name, mentor_name=mentor_name, language=language)
+        # return HttpResponse('ГОТОВО')
+    if request.method == "GET":
+        form = CourseForm()
+        return render(request, 'course_add.html', {'form': form})
+
+
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'student_list.html', {'students': students})
+
+
+def student_add(request):
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+        else:
+            return HttpResponse("Ошибка :)")
+    if request.method == "GET":
+        form = StudentForm()
+        return render(request, 'student_add.html', {'form': form})
+
+
